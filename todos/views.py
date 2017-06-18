2254c668192c42aa11cdb76299c9043313b691d2
Alex Colombo <uricaine@hotmail.com>
2017-06-18 17:30:43 +0200

    nuevas actualizaciones

--- todos/views.py
+++ todos/views.py
@@ -4,10 +4,28 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+from .models import Todo
+
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse('Hello World')
+   # return HttpResponse('Hello World')
+   
+    todos = Todo.objects.all()[:10]
 
-     
+    context = {
+        'titulo':'LISTA DE TAREAS',
+       'tareas': todos
+    }
+    return render(request, 'index.html', context)
+
+
+def details(request, id):
+    todo = Todo.objects.get(id=id)
+
+    context = {
+       'tarea': todo
+    }
+    return render(request, 'details.html', context)
